0075dd0d6f872ce8a4ec6d79d19aa88e4cba25cb
ilyassstri <trikhaqiqi@gmail.com>
2021-02-18 14:33:24 +0700

    Latihan komparasi dan logika

---
+++ 10 - Latihan Komparasi dan Logika/Main.py
@@ -0,0 +1,43 @@
+# episode latihan logika dan komparasi
+
+# membuat gabungan area rentang dari angka
+
+# +++++3--------------10+++++
+
+inputUser = float(input(
+    "Masukan angka yang bernilai\nkurang dari 3 \natau \nlebih besar dari 10\n:"))
+
+# ++++++3-------------------
+# memeriksa angka kurang dari 3
+isKurangDari = (inputUser < 3)
+print("Kurang dari 3 =", isKurangDari)
+
+# ------------------10++++++
+# Memeriksa angka lebih dari 10
+isLebihDari = (inputUser > 10)
+print(isLebihDari)
+
+# +++++3--------------10+++++
+isCorrect = isKurangDari or isLebihDari
+print("angka yang anda masukan: ", isCorrect)
+
+
+# ------3++++++++++10--------
+# kasus irisan
+print("\n", 10*"=", "\n")
+inputUser = float(input(
+    "Masukan angka yang bernilai\nlebih dari 3 \ndan \nkurang dari 10\n:"))
+
+# -----3++++++++++++++++++++
+# lebih dari 3
+isLebihDari = inputUser > 3
+print("Lebih dari 3 =", isLebihDari)
+
+# ++++++++++++++++10--------
+# kurang dari 10
+isKurangDari = inputUser < 10
+print("Kurang dari 10 =", isKurangDari)
+
+# ------3++++++++++10--------
+isCorrect = isKurangDari and isLebihDari
+print("angka yang anda masukan: ", isCorrect)
